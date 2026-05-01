0b8423b98ff022d5f29787f9e2883773b2a27370
Shadhin Khan <shadhinkhan28-6703@users.noreply.github.com>
2026-05-01 15:56:12 +0600

    set up done

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 // app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Apollo Gears World!');
+  res.send('Hello from Contest Hub!');
 });
 
 export default app;
